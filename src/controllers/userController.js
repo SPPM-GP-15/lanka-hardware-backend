5e2749e993f67a479fe0bea9b5f816bc63d76fab
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-08-06 17:47:18 +0530

    Update Changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const User = require("../models/userModel.js");
+const Product = require("../models/productModel.js");
 const generateAuthToken = require("../config/generateToken.js");
 
 const registerUser = async (req, res) => {
@@ -97,10 +98,72 @@ const updateUser = async (req, res) => {
 
     res.json({
       success: true,
-      user
+      user,
     });
   } catch (error) {
     res.status(500).send(error.message);
+  }
+};
+
+const addToWishlist = async (req, res) => {
+  const { userId, productId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+    const product = await Product.findById(productId);
+
+    if (!user || !product) {
+      return res.status(404).json({ message: "User or Product not found" });
+    }
+
+    if (!user.wishlist.includes(productId)) {
+      user.wishlist.push(productId);
+      await user.save();
+      return res.status(200).json({ message: "Product added to wishlist" });
+    } else {
+      return res.status(400).json({ message: "Product already in wishlist" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Remove a product from the user's wishlist
+const removeFromWishlist = async (req, res) => {
+  const { userId, productId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.wishlist = user.wishlist.filter((id) => id.toString() !== productId);
+    await user.save();
+
+    return res.status(200).json({ message: "Product removed from wishlist" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Get a user's wishlist
+const getWishlist = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findById(userId).populate("wishlist");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const wishlist = user.wishlist;
+
+    return res.status(200).json(wishlist);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
@@ -109,4 +172,7 @@ module.exports = {
   loginUser,
   getAllUsers,
   updateUser,
+  addToWishlist,
+  removeFromWishlist,
+  getWishlist,
 };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,6 +4,9 @@ const {
   loginUser,
   getAllUsers,
   updateUser,
+  addToWishlist,
+  removeFromWishlist,
+  getWishlist,
 } = require("../controllers/userController");
 const router = express.Router();
 
@@ -11,5 +14,7 @@ router.post("/signup", registerUser);
 router.post("/login", loginUser);
 router.post("/getAllUsers", getAllUsers);
 router.put("/updateUser/:userId", updateUser);
-
+router.post("/wishlist/add/:userId/:productId", addToWishlist);
+router.delete("/wishlist/remove/:userId/:productId", removeFromWishlist);
+router.get("/wishlist/:userId", getWishlist);
 module.exports = router;
