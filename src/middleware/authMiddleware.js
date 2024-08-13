130c405590581eab8604d4c18e7c0aa4635cf0dd
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-08-13 14:01:23 +0530

    Update Changes

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -14,7 +14,7 @@ const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
       // Get user from the token
       req.user = await User.findById(decoded.id).select("-password");
